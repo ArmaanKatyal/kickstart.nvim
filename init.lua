8fa629441b8695d9a55ad5c0766ec38b7bac417a
Armaan Katyal <armaankatyal3@gmail.com>
2024-04-13 14:54:13 -0600

    fix: merge conflict

--- init.lua
+++ init.lua
@@ -761,17 +761,7 @@ require('lazy').setup({
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
     --
-<<<<<<< Updated upstream
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
-=======
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'loctvl842/monokai-pro.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
@@ -822,7 +812,6 @@ require('lazy').setup({
       }
       -- Load the colorscheme here
       vim.cmd.colorscheme 'monokai-pro'
->>>>>>> Stashed changes
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
